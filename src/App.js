b78d95c2f4fd0de0aaf52731fd6f8a90368ad068
VYOM PUNDHIR <vyom.pundhir.prof@gmail.com>
2023-03-07 23:12:15 +0530

    Project Trial completed.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./style.css";
+import Button from "./Button";
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1 style={{textAlign:"center"}}>Hello World !!!</h1>
+      <Button title="App Store"/>
+      <Button title="Play Store"/>
+      <Button />
+      {/* <button className="button">App Store</button>
+      <button className="button">Play Store</button> */}
     </div>
+    
   );
 }
 
---
+++ src/Button.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Button = ({title = "Nothing" }) => (
+  <div>
+    <button className="button">{title}</button>
+  </div>
+  
+);
+
+export default Button;
